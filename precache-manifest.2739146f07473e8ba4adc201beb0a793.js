b41c26d0275623a5398955c82eda7663ece78bc8
김민혁 <minhyeok95@ajou.ac.kr>
2022-07-04 11:15:26 +0900

    Updates

---
+++ precache-manifest.2739146f07473e8ba4adc201beb0a793.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3be9a4d58549ff20a8bb",
+    "url": "/react-practice/static/js/main.3be9a4d5.chunk.js"
+  },
+  {
+    "revision": "91098b0328cb09313dc7",
+    "url": "/react-practice/static/js/1.91098b03.chunk.js"
+  },
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "3aa322e6be8f99cf30b461b7f211cb64",
+    "url": "/react-practice/index.html"
+  }
+];
